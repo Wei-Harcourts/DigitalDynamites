08deecd5720e921dc15a8aeb1365c2855b15a518
bsundaram <bala.sundaram@harcourts.net>
2016-08-18 11:33:04 +1200

    Facebook integration completed(Auth and pulling user information)

---
+++ Harcourts.eOpen.Web/Harcourts.eOpen.Web/Models/FacebookUserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Harcourts.eOpen.Web.Models
+{
+    public class FacebookUserModel
+    {
+        public string id { get; set; }
+
+        public string email { get; set; }
+
+        public string first_name { get; set; }
+
+        public string last_name { get; set; }
+
+        public FacebookOAuthResponseModel AuthResponseModel { get; set; }
+
+    }
+}
---
+++ Harcourts.eOpen.Web/Harcourts.eOpen.Web/Settings/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace Harcourts.eOpen.Web.Settings
+{
+    public class AppSettings
+    {
+        public AppSettings()
+        {
+            ClientId = ConfigurationManager.AppSettings["AppClientId"];
+            ClientSecret = ConfigurationManager.AppSettings["AppClientSecret"];
+            RedirectUri = ConfigurationManager.AppSettings["AppRedirectUri"];
+        }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string RedirectUri { get; set; }
+    }
+}
