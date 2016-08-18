5d9a7fc757b6ae9e632ae8795f3032d298bfc2e3
mitchell.demler <mitchell.demler@harcourts.net>
2016-08-18 16:12:04 +1200

    Setup dummy controller and sass

---
+++ Harcourts.eOpen.Web/Harcourts.eOpen.Web/Gruntfile.js
@@ -0,0 +1,52 @@
+﻿/**
+ * Configure Grunt - http://benfrain.com/lightning-fast-sass-compiling-with-libsass-node-sass-and-grunt-sass/
+ */
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            sass: {
+                files: [
+                    'Scss/{,*/}*.{scss,sass}'
+                ],
+                tasks: ['sass:dist']
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    includePaths: [
+                        require('node-bourbon').includePaths,
+                        "node_modules/font-awesome/scss/"
+                    ],
+                    check: false,
+                    precision: 10,
+                    outputStyle: "compressed"
+                },
+                files: {
+                    'Content/Site.css': 'Scss/main.scss'
+                }
+            }
+        },
+        concat: {
+            thirdparty: {
+                files: [{
+                    dest: 'Content/thirdparty.min.js',
+                    src: [
+                        'node_modules/fullpage.js/jquery.fullPage.js'
+                    ]
+                }]
+            }
+        }
+    });
+    
+
+    grunt.registerTask('default', [
+        'sass',
+        'concat:thirdparty'
+    ]);
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
